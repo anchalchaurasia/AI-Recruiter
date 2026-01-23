8dc911b337213b16240b96f54f8c539c9e67960b
anchalchaurasia <anchalchaurasia2005@gmail.com>
2026-01-23 22:45:21 +0530

    Fix: clean install & secure Next.js version

--- app/page.js
+++ app/page.js
@@ -5,10 +5,7 @@ export default function Home() {
   return (
     <div>
       <h2>Made By Anchal Chaurasia</h2>
-      <Button>
-        <a  href="https://ai-recruiter-olmy.vercel.app/dashboard">Go to The Dashbaord</a>
-      </Button>
-      <h2>OR</h2>
+     
       <Button>
         <a href="https://ai-recruiter-olmy.vercel.app/auth">Go to The login page</a>
       </Button>
