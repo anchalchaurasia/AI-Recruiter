7edf348276e1e18733e4b690c8ae7e49e85e33b5
anchalchaurasia <anchalchaurasia2005@gmail.com>
2026-01-23 22:30:06 +0530

    Fix: clean install & secure Next.js version

--- app/page.js
+++ app/page.js
@@ -4,13 +4,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>Made By Anchal Chaauurasia</h2>
+      <h2>Made By Anchal Chaurasia</h2>
       <Button>
         <a  href="https://ai-recruiter-olmy.vercel.app/dashboard">Go to The Dashbaord</a>
       </Button>
       <h2>OR</h2>
       <Button>
-        <a  href="https://ai-recruiter-olmy.vercel.app/auth">Go to The login page</a>
+        <a href="https://ai-recruiter-olmy.vercel.app/auth">Go to The login page</a>
       </Button>
     </div>
   );
