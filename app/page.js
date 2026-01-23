c90510d24273fceca232122d834cd3d07f775f79
anchalchaurasia <anchalchaurasia2005@gmail.com>
2026-01-23 22:24:29 +0530

    Fix: clean install & secure Next.js version

--- app/page.js
+++ app/page.js
@@ -4,8 +4,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>Anchal Chaurasia page.js</h2>
-      <Button>Subscribe</Button>
+      <h2>Made By Anchal Chaauurasia</h2>
+      <Button>
+        <a  href="https://ai-recruiter-olmy.vercel.app/dashboard">Go to The Dashbaord</a>
+      </Button>
     </div>
   );
 }
